712bae530bd1cb9b734e4ec2d78553930ffea1fd
amtjitro <60487970+amtjitro@users.noreply.github.com>
2022-01-27 18:46:33 -0800

    Update MarkdownParse.java
    edited to work for image files

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,15 +18,17 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(nextOpenBracket > 0 && markdown.charAt(nextOpenBracket - 1) != '!'){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
+		    Path fileName = Path.of(args[0]);
+	        String contents = Files.readString(fileName);
+            ArrayList<String> links = getLinks(contents);
+            System.out.println(links);
     }
 }
