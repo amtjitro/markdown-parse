038382003c8d0c6d33a2206db6ce40b6af208cb4
amtjitro <60487970+amtjitro@users.noreply.github.com>
2022-02-23 17:09:50 -0800

    edits for lab report 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -42,4 +42,22 @@ public class MarkdownParseTest {
         assertEquals(List.of("https://google.com"), MarkdownParse.getLinks(Files.readString(Path.of("test-file5.md"))));
     }
 
+    @Test
+    public void testSnippet1() throws IOException{
+        assertEquals(List.of("'google.com, google.com, ucsd.edu"),
+            MarkdownParse.getLinks(Files.readString(Path.of("snippet1.md"))));
+    }
+
+    @Test
+    public void testSnippet2() throws IOException{
+        assertEquals(List.of("a.com, a.com(()), example.com"),
+            MarkdownParse.getLinks(Files.readString(Path.of("snippet2.md"))));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException{
+        assertEquals(List.of("https://www.twitter.com, https://ucsd-cse15l-w22.github.io/, https://cse.ucsd.edu/"),
+            MarkdownParse.getLinks(Files.readString(Path.of("snippet3.md"))));
+    }
+
 }
